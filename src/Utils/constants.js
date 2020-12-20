509278898d31c1b5f40c0f3a212059d621a77b03
Ian <ianng2000@gmail.com>
2020-12-21 09:16:58 +1030

    env

--- src/Utils/constants.js
+++ src/Utils/constants.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 export const CLIENT_ID = process.env.CLIENT_ID
 export const CLIENT_SECRET = process.env.CLIENT_SECRET
 export const REDIRECT_URI = "https://discover-daily.herokuapp.com/lmaohelp"
